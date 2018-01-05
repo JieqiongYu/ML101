0d34feeaa2516debed9e015aa7bbdd06c5984bdd
Yu Jieqiong <yjieqiong@gmail.com>
2018-01-05 16:18:45 +0900

    Splitting the Dataset into the Training set and Test set

--- Data Preprocessing/data_preprocessing_template.py
+++ Data Preprocessing/data_preprocessing_template.py
@@ -12,7 +12,10 @@ import pandas as pd
 
 # Importing the dataset
 dataset = pd.read_csv('Data.csv')
+# For Python, index starts from 0
+# X takes all the columns except the last one
 X = dataset.iloc[:, :-1].values
+# y takes the last column
 y = dataset.iloc[:, 3].values
 
 # Taking care of missing data
@@ -25,7 +28,13 @@ X[:, 1:3] = imputer.transform(X[:, 1:3])
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 labelencoder_X = LabelEncoder()
 X[:, 0] = labelencoder_X.fit_transform(X[:, 0])
-# categorical_featuers = [0] means the first column, 
+# categorical_featuers = [0] means the first column,  
 # 0 is the index number of the target column
 onehotencoder = OneHotEncoder(categorical_features = [0])
 X = onehotencoder.fit_transform(X).toarray()
+labelendcoder_Y = LabelEncoder()
+y = labelendcoder_Y.fit_transform(y)
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.cross_validation import train_test_split 
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
